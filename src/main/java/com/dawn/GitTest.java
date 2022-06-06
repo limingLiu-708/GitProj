eda9f70ab25a59e99d500ac177b01e7fe62a8b62
limingLiu-708 <limingliu6@163.com>
2022-06-06 15:31:09 +0800

    another branch commit

--- src/main/java/com/dawn/GitTest.java
+++ src/main/java/com/dawn/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Hello, idea and git");
-
+        System.out.println("another branch commit");
 
     }
 
