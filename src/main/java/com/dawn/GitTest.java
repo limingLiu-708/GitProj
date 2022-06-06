676998753c49888c4c79d94fa89e30ef9782111b
limingLiu-708 <limingliu6@163.com>
2022-06-06 18:30:50 +0800

    master add one line

--- src/main/java/com/dawn/GitTest.java
+++ src/main/java/com/dawn/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, idea and git");
         System.out.println("another branch commit");
-
+        System.out.println("master add one line");
     }
 
 
